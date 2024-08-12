cb0b2387381dac1f38990d7add158dac2b796e41
joelberto <kauanfvale@gmail.com>
2024-08-12 08:42:25 -0300

    🚧in progress: first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [cards, setCards] = useState(
+    [ { id: 0, image: "railsurl", cardName: "RoR" },
+    { id: 1, image: "javascripturl", cardName: "JavaScript" } ]
+
+  )
   return (
     <>
       <h1 className="bg-red-300">Banana</h1>
