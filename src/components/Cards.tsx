2acacbb008fc9f7fad617c4ebd235599ac59614c
kaufon <kauanfvale@gmail.com>
2024-08-14 20:29:57 -0300

    💄style: finishing touchs

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,49 +4,49 @@ function Cards() {
   const [cards, setCards] = useState([
     {
       id: 0,
-      cardImage: "../../public/deergirl2.png",
+      cardImage: "/deergirl2.png",
       cardName: "DeerGirl2",
       isClicked: false,
     },
     {
       id: 1,
-      cardImage: "../../public/shikano1.png",
+      cardImage: "/shikano1.png",
       cardName: "deergirl1",
       isClicked: false,
     },
     {
       id: 2,
-      cardImage: "../../public/deergirl3.png",
+      cardImage: "/deergirl3.png",
       cardName: "deergirl3",
       isClicked: false,
     },
     {
       id: 3,
-      cardImage: "../../public/deergirl4.png",
+      cardImage: "/deergirl4.png",
       cardName: "deergirl4",
       isClicked: false,
     },
     {
       id: 4,
-      cardImage: "../../public/deergirl5.png",
+      cardImage: "/deergirl5.png",
       cardName: "deergirl5",
       isClicked: false,
     },
     {
       id: 5,
-      cardImage: "../../public/blondgirl1.png",
+      cardImage: "/blondgirl1.png",
       cardName: "blondgirl1",
       isClicked: false,
     },
     {
       id: 6,
-      cardImage: "../../public/blondgirl2.png",
+      cardImage: "/blondgirl2.png",
       cardName: "blondgirl2",
       isClicked: false,
     },
     {
       id: 7,
-      cardImage: "../../public/blodengirl3.png",
+      cardImage: "/blodengirl3.png",
       cardName: "blondgirl3",
       isClicked: false,
     },
@@ -90,17 +90,30 @@ function Cards() {
     <>
       <div className="space-y-2">
         <div className="flex justify-between bg-black w-full h-24 text-white items-center">
-          <h1 className="text-2xl p-20">{score}</h1>
-          <h2>seu maior ponto foi {highestScore}</h2>
+          <h1 className="text-2xl p-5 bg-gray-800 border-4 border-gray-900 font-bold  italic   rounded-full text-gray-300 ">
+            Streak: {score}
+          </h1>
+          <h1 className="text-2xl p-5 bg-gray-800 border-4 border-gray-900 font-bold  italic   rounded-full text-gray-300">
+            Top Points: {highestScore}
+          </h1>
         </div>
-        <div className="space-y-2 flex flex-row flex-1 justify-center items-center gap-5">
-          {cards.map((card) => (
-            <h1 key={card.cardName}>
-              <button onClick={clickCard} name={card.cardName}>
-                <img src={card.cardImage} alt={card.cardName} />
+        <div className="flex justify-center items-center min-h-screen ">
+          <div className="grid grid-cols-3 gap-3  ">
+            {cards.map((card) => (
+              <button
+                onClick={clickCard}
+                name={card.cardName}
+                key={card.cardName}
+                className=" "
+              >
+                <img
+                  src={card.cardImage}
+                  alt={card.cardName}
+                  className="w-64 h-56 rounded-full border-4 border-orange-950"
+                />
               </button>
-            </h1>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </>
